65f6d891a8397d4dbdaaf04b9d515bf43a897470
levc4 <levc4kvlad@gmail.com>
2024-03-07 17:13:17 +0200

    Added 415 to documentation in response list for POST/econews/uploadImages

--- core/src/main/java/greencity/constant/HttpStatuses.java
+++ core/src/main/java/greencity/constant/HttpStatuses.java
@@ -7,6 +7,7 @@ public final class HttpStatuses {
     public static final String BAD_REQUEST = "Bad Request";
     public static final String UNAUTHORIZED = "Unauthorized";
     public static final String FORBIDDEN = "Forbidden";
+    public static final String UNSUPPORTED_MEDIA_TYPE = "Unsupported media type";
     public static final String NOT_FOUND = "Not Found";
     public static final String SEE_OTHER = "See Other";
     public static final String INTERNAL_SERVER_ERROR = "INTERNAL_SERVER_ERROR";
